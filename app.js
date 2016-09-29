03e1f00d699a95d6ff7255dbbd4f60e3011bf9a9
Attax <westooy@gmail.com>
2016-09-29 11:08:56 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const mysql=require('mysql');
+const Redis=require('redis');
+const async=require('async');
+
+var app=new express();
+
+app.use('/',function(){
+  
+});
